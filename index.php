0ff0e957cebea1ceab88569ef8de93b24324cc91
ammar alamudi <muhammadadiblutfi11@gmail.com>
2020-06-22 23:34:03 +0700

    task2

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 //date untuk menampilkan tanggal dengan format tertentu
-	echo date ("1");
+echo date(
+	"1, d-M-Y"),
+	time () +
+		50 * 60 *24 + 100
+);
+// dengan format tersebut kita dapat mengetahui 100 hari yang akan datang hari apa		
 
 	?>
