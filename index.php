cab745a80d04aaa93f1b25d203392dc69d29ec5b
ammar alamudi <muhammadadiblutfi11@gmail.com>
2020-06-22 23:45:11 +0700

    task3

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 echo date(
 	"1, d-M-Y"),
 	time () +
-		50 * 60 *24 + 100
+		50 * 60 *24 + 1000
 );
-// dengan format tersebut kita dapat mengetahui 100 hari yang akan datang hari apa		
+// dengan format tersebut kita dapat mengetahui 1000 hari yang akan datang hari apa		
 
 	?>
